c60d35ca8586a65f0d8873792a5c0f4857c052ba
Tomás Arjovsky <t.arjovsky@gmail.com>
2019-08-12 02:52:43 -0300

    Añado link en index

--- tests/app/test_index.py
+++ tests/app/test_index.py
@@ -8,3 +8,8 @@ def client():
 def test_index(client):
     rv = client.get('/')
     assert '2019 - 1° cuatrimestre' in rv.data.decode('utf-8')
+
+def test_link(client):
+    rv = client.get('/').data.decode('utf-8')
+    assert 'a href="/periodo?a=2019' in rv
+    assert 'c=1' in rv
