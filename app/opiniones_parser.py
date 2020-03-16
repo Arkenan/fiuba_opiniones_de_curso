472609cc72ad3db9c89b1789741037f7a49e1e80
Tomás Arjovsky <t.arjovsky@gmail.com>
2020-03-15 21:45:05 -0300

    Añado archivo del segundo cuatri
    - Cambio "Elige un curso" por "Curso"
    - Aplico Black al test del parser.
    - Dejo en comentarios el test final y uno en progreso.

--- app/opiniones_parser.py
+++ app/opiniones_parser.py
@@ -6,59 +6,64 @@ from modelo.nivel_clases import NivelClases
 from modelo.dificultad import Dificultad
 from app.excepcion_curso_no_valido import ExcepcionCursoNoValido
 
+
 class OpinionesParser:
-    TEXTO = 'Comentarios Sobre el Curso'
+    TEXTO = "Comentarios Sobre el Curso"
     SEPARADORES = "-,"
 
     def __init__(self, nombre_archivo):
-        self.csv = csv.DictReader(open(nombre_archivo), delimiter = ',')
+        self.csv = csv.DictReader(open(nombre_archivo), delimiter=",")
 
     def next(self):
         csv_opinion = next(self.csv)
         asignatura, curso = self.get_curso(csv_opinion)
         return Opinion(
-            asignatura = asignatura, curso = curso,
-            aprobo = self.aprobo(csv_opinion),
-            interes = self.interes(csv_opinion),
-            general = self.general(csv_opinion),
-            actual = self.actual(csv_opinion),
-            nivel_teoricas = self.nivel_teoricas(csv_opinion),
-            nivel_practicas = self.nivel_practicas(csv_opinion),
-            dificultad_curso = self.dificultad_curso(csv_opinion),
-            dificultad_tp = self.dificultad_tp(csv_opinion),
-            texto = csv_opinion[self.TEXTO])
+            asignatura=asignatura,
+            curso=curso,
+            aprobo=self.aprobo(csv_opinion),
+            interes=self.interes(csv_opinion),
+            general=self.general(csv_opinion),
+            actual=self.actual(csv_opinion),
+            nivel_teoricas=self.nivel_teoricas(csv_opinion),
+            nivel_practicas=self.nivel_practicas(csv_opinion),
+            dificultad_curso=self.dificultad_curso(csv_opinion),
+            dificultad_tp=self.dificultad_tp(csv_opinion),
+            texto=csv_opinion[self.TEXTO],
+        )
 
     def get_curso(self, csv_opinion):
-        curso_csv = csv_opinion['Elige el curso']
+        curso_csv = csv_opinion["Curso"]
         for sep in self.SEPARADORES:
             if sep in curso_csv:
                 asignatura, curso = curso_csv.split(sep)
                 return asignatura.strip(), curso.strip()
-        raise ExcepcionCursoNoValido(csv_opinion['Elige el curso'])
+        raise ExcepcionCursoNoValido(csv_opinion["Elige el curso"])
 
     def aprobo(self, csv_opinion):
-        return csv_opinion['¿Aprobó la Cursada?'].lower() == 'sí'
+        return csv_opinion["¿Aprobó la Cursada?"].lower() == "sí"
 
     def actual(self, csv_opinion):
-        return csv_opinion['¿Los Temas de la Materia Están Actualizados?'].lower() == 'sí'
+        return (
+            csv_opinion["¿Los Temas de la Materia Están Actualizados?"].lower() == "sí"
+        )
 
     def interes(self, csv_opinion):
-        return Interes(csv_opinion['¿Cómo te Resultaron los Temas de la Materia?'])
+        return Interes(csv_opinion["¿Cómo te Resultaron los Temas de la Materia?"])
 
     def general(self, csv_opinion):
-        return OpinionGeneral(csv_opinion['Opinión General Sobre el Curso'])
+        return OpinionGeneral(csv_opinion["Opinión General Sobre el Curso"])
 
     def nivel_teoricas(self, csv_opinion):
-        return NivelClases(csv_opinion['Nivel de las Clases Teóricas del Curso'])
+        return NivelClases(csv_opinion["Nivel de las Clases Teóricas del Curso"])
 
     def nivel_practicas(self, csv_opinion):
-        return NivelClases(csv_opinion['Nivel de las Clases Prácticas del Curso'])
+        return NivelClases(csv_opinion["Nivel de las Clases Prácticas del Curso"])
 
     def dificultad_curso(self, csv_opinion):
-        return Dificultad(csv_opinion['Dificultad del Curso'])
+        return Dificultad(csv_opinion["Dificultad del Curso"])
 
     def dificultad_tp(self, csv_opinion):
-        return Dificultad(csv_opinion['Dificultad del TP'])
+        return Dificultad(csv_opinion["Dificultad del TP"])
 
     def __iter__(self):
         return self
--- tests/app/test_opiniones_parser.py
+++ tests/app/test_opiniones_parser.py
@@ -2,63 +2,87 @@ import pytest
 import csv
 from app.opiniones_parser import OpinionesParser
 
+
 def test_should_fail_if_file_not_found():
     with pytest.raises(FileNotFoundError):
-        parser = OpinionesParser('archivo_falso.csv')
+        parser = OpinionesParser("archivo_falso.csv")
+
 
 def test_materia_y_curso_separados_guion():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
-    assert opinion.asignatura == 'Análisis Numérico'
-    assert opinion.curso == 'Schwarz'
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
+    assert opinion.asignatura == "Análisis Numérico"
+    assert opinion.curso == "Schwarz"
+
 
 def test_materia_y_curso_separados_coma():
-    parser = OpinionesParser('tests/app/encuesta.csv')
+    parser = OpinionesParser("tests/app/encuesta.csv")
     parser.next()
     opinion = parser.next()
-    assert opinion.asignatura == 'Algoritmos y Programación I'
-    assert opinion.curso == 'Costa'
+    assert opinion.asignatura == "Algoritmos y Programación I"
+    assert opinion.curso == "Costa"
+
 
 def test_materia_y_curso_guien_sin_espacio():
-    opinion = OpinionesParser('tests/app/encuesta_guion_sin_espacio.csv').next()
-    assert opinion.asignatura == 'Computación'
-    assert opinion.curso == 'Calvo'
+    opinion = OpinionesParser("tests/app/encuesta_guion_sin_espacio.csv").next()
+    assert opinion.asignatura == "Computación"
+    assert opinion.curso == "Calvo"
+
 
 def test_aprobo():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
     assert opinion.aprobo
 
+
 def test_interes():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
     assert opinion.interes.puntos == 4
-    assert opinion.interes.texto == 'Muy Interesantes'
+    assert opinion.interes.texto == "Muy Interesantes"
+
 
 def test_opinion_general():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
     assert opinion.general.puntos == 5
     assert opinion.general.texto == "Excelente"
 
+
 def test_actual():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
     assert opinion.actual
 
+
 def test_nivel_clases():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
     assert opinion.nivel_teoricas.puntos == 3
     assert opinion.nivel_practicas.puntos == 5
 
+
 def test_dificultad():
-    opinion = OpinionesParser('tests/app/encuesta.csv').next()
+    opinion = OpinionesParser("tests/app/encuesta.csv").next()
     assert opinion.dificultad_curso.puntos == 4
     assert opinion.dificultad_tp.puntos == 3
 
+
 def test_texto():
-    parser = OpinionesParser('tests/app/encuesta.csv')
+    parser = OpinionesParser("tests/app/encuesta.csv")
     opinion = parser.next()
-    assert 'Creo que' in opinion.texto
+    assert "Creo que" in opinion.texto
     opinion = parser.next()
-    assert opinion.texto == ''
+    assert opinion.texto == ""
+
 
 # No falla leer las opiniones del 2019-1C.
 def test_2019_1C():
-    parser = OpinionesParser('data/encuesta-dc-2019-1C.csv')
+    parser = OpinionesParser("data/encuesta-dc-2019-1C.csv")
     [opinion for opinion in parser]
+
+
+# def test_formato_doble_codigo_materia_curso_docente():
+#     parser = OpinionesParser("test/app/formatos.csv")
+#     opinion = parser.next()
+#     "75.01-95.01-Computación - 1 - Strobino"
+
+# No falla leer las opiniones del 2019-2C.
+# def test_2019_2C():
+#     parser = OpinionesParser("data/encuesta-dc-2019-2C.csv")
+#     [opinion for opinion in parser]
+#
